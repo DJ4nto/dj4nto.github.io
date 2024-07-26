e60e8690232281264d3db78c013d019a3cad6876
Antonin LECOCQ <lecocqantonin@gmail.com>
2024-07-27 01:32:18 +0200

    bug fixed

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ let root = document.documentElement;
     
 root.addEventListener('mousemove', e => {
   root.style.setProperty('--mouse-x', e.clientX + "px");
-  root.style.setProperty('--mouse-y', e.clientY + "px");
+  root.style.setProperty('--mouse-y', e.clientY-26 + "px");
 });
 
 document.addEventListener("DOMContentLoaded", function() {
